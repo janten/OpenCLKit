1e5a8361e152ad67036e9374e46a5c63a17381bf
Jan-Gerd Tenberge <janten@gmail.com>
2015-04-14 13:04:22 +0200

    Initial Commit

---
+++ OpenCLKit/OpenCLKit.h
@@ -0,0 +1,19 @@
+//
+//  OpenCLKit.h
+//  OpenCLKit
+//
+//  Created by Jan-Gerd Tenberge on 14.04.15.
+//  Copyright (c) 2015 Jan-Gerd Tenberge. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OpenCLKit.
+FOUNDATION_EXPORT double OpenCLKitVersionNumber;
+
+//! Project version string for OpenCLKit.
+FOUNDATION_EXPORT const unsigned char OpenCLKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenCLKit/PublicHeader.h>
+
+
